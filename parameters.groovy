c0e94374a657279525362b5548157586e21872e0
sunilnun <sunilnune38@gmail.com>
2024-08-21 21:25:20 +0530

    deleted

--- parameters.groovy
+++ parameters.groovy
@@ -22,21 +22,20 @@ pipeline {
 pipeline{
     agent any
     parameters{
-        string (name: 'change ticket number', defaultValue: 'ITCHG12345', description: 'enter the change request number')
-        text (name: 'which environment', defaultValue: 'dev', description: 'which environment you are deploying')
-        boolparam (name: 'is sre approved', defaultValue: 'true' description: 'is approval taken from the SRE')
-        choice (choices: 'regular/hotfix/main/feature', name: 'Regular', description: 'what release is this')
-        password (name: 'my password', defaultValue:'' , description: 'enter your password')
-        credentials (name: 'my creds', description: 'my docker credentials', required: 'true')    
+        string (name: 'change_ticket_number', defaultValue: 'ITCHG12345', description: 'enter the change request number')
+        text (name: 'which_environment', defaultValue: 'dev', description: 'which environment you are deploying')
+        choice (choices: 'regular\nhotfix\nmain\nfeature', name: 'Regular', description: 'what release is this')
+        password (name: 'my_password', defaultValue:'' , description: 'enter your password')
+        credentials (name: 'my_creds', description: 'my docker credentials', required: 'true')    
     }
     stages{
         stage('parameters stage'){
             steps{
-                echo "your change ticket number is ${params.change ticket number}"
-                echo "you are deploying to ${params.which environment}"
+                echo "your change ticket number is ${params.change_ticket_number}"
+                echo "you are deploying to ${params.which_environment}"
                 echo "you are deploying from ${params.Regular} branch"
-                echo "your password is ${params.my password}"
-                echo "selected credentials are ${params.my creds}"
+                echo "your password is ${params.my_password}"
+                echo "selected credentials are ${params.my_creds}"
             }
         }
     }
